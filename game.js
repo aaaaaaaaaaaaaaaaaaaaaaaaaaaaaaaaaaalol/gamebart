3e86f16433d91d8aa1d69ded8b6da317868cdee1
aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaalol <simon.garnish@bartcharter.org>
2025-12-03 08:45:38 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,17 @@
-
+/Script start
+let board = [["c","c","c"],
+      	 	 [" "," "," "],
+             ["o","o","o"]];
+let sx = 0;
+let sy = 0;
+function setToBoard(){
+	let st = "";
+	for(let i = 0;i<board.length;i++){
+		for(let j = 0;j<board[i].length;j++){
+			st+=board[i][j];
+		}
+		st+="<br>"
+	}
+	return st;
+}
+document.getElementById("Board").innerHtml = setToBoard();
