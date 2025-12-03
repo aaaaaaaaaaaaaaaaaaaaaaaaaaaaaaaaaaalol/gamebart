cdfb266a86c1f172ac9db5636c26c1c599462c73
aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaalol <simon.garnish@bartcharter.org>
2025-12-03 08:37:04 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,16 +1,16 @@
 //Script start
-      let board = [["c","c","c"],
-                   [" "," "," "],
-                   ["o","o","o"]];
-      let sx = 0;
-      let sy = 0;
-      document.getElementById()
-      document.body.onkeyup = function(e) {
-  if (e.key == " " ||
-      e.code == "Space" ||      
-      e.keyCode == 32      
-  ) {
-    board[sx][sy].toUpperCase
-  }
+let board = [["c","c","c"],
+      	 [" "," "," "],
+             ["o","o","o"]];
+let sx = 0;
+let sy = 0;
+function setToBoard(){
+	for(let i = 0;i<board.length;i++){
+		for(let j = 0;j<board[j].length;j++){
+			
+		}
+	}
 }
+document.getElementById("Board").innerHtml = setToBoard()
+
 //Script End
